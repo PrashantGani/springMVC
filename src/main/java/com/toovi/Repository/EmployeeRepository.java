598cade945d4a51811f387bc33ddafa17e509170
PrashantGani <prashantgani005@gmail.com>
2023-07-09 15:27:13 +0530

    intial commit

---
+++ src/main/java/com/toovi/Repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.toovi.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.toovi.Entity.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+ 
+}
